e5659195050c2725e99043aa8e43e5fc713462e9
anafletcher <153273057+anafletcher@users.noreply.github.com>
2024-03-17 12:09:48 +0000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Alicia Fletcher and is{" "}
+          <a
+            href="https://updated-react-weather-app.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
